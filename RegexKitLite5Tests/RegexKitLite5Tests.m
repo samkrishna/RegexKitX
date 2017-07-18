394a756213208f97b50f582b90a21dc644a0102f
Sam Krishna <sam.krishna@gmail.com>
2017-07-17 21:55:17 -0700

    Minor edit

--- RegexKitLite5Tests/RegexKitLite5Tests.m
+++ RegexKitLite5Tests/RegexKitLite5Tests.m
@@ -5,7 +5,6 @@
 //  Created by Sam Krishna on 6/12/17.
 //  Copyright © 2017 Sam Krishna. All rights reserved.
 //
-
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
 //  in the Software without restriction, including without limitation the rights
