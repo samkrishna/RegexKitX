266304b3b272d3f384b9c31884b38a7c2f374ced
Sam Krishna <sam.krishna@gmail.com>
2017-09-30 08:08:38 -0700

    Swift: Removed 'sausage-making' Range->NSRange API

--- SwiftRegexKitX/RegexKitX.swift
+++ SwiftRegexKitX/RegexKitX.swift
@@ -159,16 +159,6 @@ public extension String {
         return NSRange(range!, in: self)
     }
 
-    func sausageMakingLegacyNSRange(from range: Range<String.UTF16Index>?)
-        -> NSRange {
-            guard range != nil else { return NSMakeRange(NSNotFound, 0) }
-            let lowerBound = range?.lowerBound.samePosition(in: utf16)
-            let upperBound = range?.upperBound.samePosition(in: utf16)
-            let location = utf16.distance(from: utf16.startIndex, to: lowerBound!)
-            let length = utf16.distance(from: lowerBound!, to: upperBound!)
-            return NSMakeRange(location, length)
-    }
-
     // MARK: Internal
     fileprivate static func cacheKeyFor(_ pattern: String, options: RKXRegexOptions)
         -> String {
