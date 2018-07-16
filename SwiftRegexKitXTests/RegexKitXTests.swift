8eaea2dc34d33049ed6becc369b7409083cff6fd
Sam Krishna <sam.krishna@gmail.com>
2018-07-16 09:50:20 -0700

    [Swift] Deprecated setUp() and tearDown() since they weren't being used

--- SwiftRegexKitXTests/RegexKitXTests.swift
+++ SwiftRegexKitXTests/RegexKitXTests.swift
@@ -7,16 +7,6 @@ import XCTest
 
 class RegexKitXTests: XCTestCase {
     public let candidate = "2014-05-06 17:03:17.967 EXECUTION_DATA: -1 EUR EUR.JPY 14321016 orderId:439: clientId:75018, execId:0001f4e8.536956da.01.01, time:20140506  17:03:18, acctNumber:DU161169, exchange:IDEALPRO, side:SLD, shares:141500, price:141.73, permId:825657452, liquidation:0, cumQty:141500, avgPrice:141.73";
-
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
     
     func testMatchesRegex() {
         let regex = "(.*) EXECUTION_DATA: .* (\\w{3}.\\w{3}) .* orderId:(\\d+): clientId:(\\w+), execId:(.*.01), .*, acctNumber:(\\w+).*, side:(\\w+), shares:(\\d+), price:(.*), permId:(\\d+).*"
