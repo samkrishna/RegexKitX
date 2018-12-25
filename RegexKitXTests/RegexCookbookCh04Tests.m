b1b9a087ec27a148a780e7dece119181384d09a2
Sam Krishna <sam.krishna@gmail.com>
2018-12-25 07:51:26 -0800

    [Objective-C] Start RXCB Ch04 tests
    - RXCB is my acronym for Regular eXpressions Cook Book

---
+++ RegexKitXTests/RegexCookbookCh04Tests.m
@@ -0,0 +1,32 @@
+//
+//  RegexCookbookCh04Tests.m
+//  RegexKitXTests
+//
+//  Created by Sam Krishna on 12/25/18.
+//  Copyright © 2018 Sam Krishna. All rights reserved.
+//
+
+#import "RegexKitX.h"
+#import <XCTest/XCTest.h>
+
+@interface RegexCookbookCh04Tests : XCTestCase
+
+@end
+
+@implementation RegexCookbookCh04Tests
+
+- (void)testRegexFromSection41
+{
+    NSString *testEmail = @"hello.from@example.com";
+    XCTAssertTrue([testEmail isMatchedByRegex:@"^\\S+@\\S+$"]);
+    XCTAssertTrue([testEmail isMatchedByRegex:@"^[A-Z0-9+_.-]+@[A-Z0-9.-]+$" options:RKXCaseless]);
+    XCTAssertTrue([testEmail isMatchedByRegex:@"^[A-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[A-Z0-9.-]+$" options:RKXCaseless]);
+
+    NSString *noLeadingTrailingOrConsecutiveDots = @"^[A-Z0-9_!#$%&'*+/=?`{|}~^-]+(?:\\.[A-Z0-9_!#$%&'*+/=?`{|}~^-]+)*@[A-Z0-9-]+(?:\\.[A-Z0-9-]+)*$";
+    XCTAssertTrue([testEmail isMatchedByRegex:noLeadingTrailingOrConsecutiveDots options:RKXCaseless]);
+
+    NSString *tldHas2to6LettersRegex = @"^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[A-Z0-9-]+\\.)+[A-Z]{2,6}$";
+    XCTAssertTrue([testEmail isMatchedByRegex:tldHas2to6LettersRegex options:RKXCaseless]);
+}
+
+@end
