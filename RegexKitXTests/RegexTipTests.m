63806ff8e004b700e02d022e74cefb0699f9f1cf
Sam Krishna <sam.krishna@gmail.com>
2019-08-27 18:25:18 -0700

    [Objective-C] Add regex that tests primality of numbers

--- RegexKitXTests/RegexTipTests.m
+++ RegexKitXTests/RegexTipTests.m
@@ -26,4 +26,26 @@
     }
 }
 
+- (void)test20190812Tip
+{
+    // A regex that tests numbers for prime numbers:
+    // https://www.noulakaz.net/2007/03/18/a-regular-expression-to-check-for-prime-numbers/
+    BOOL(^isPrime)(NSUInteger) = ^BOOL(NSUInteger testNumber) {
+        NSString *candidate = [@"1" stringByPaddingToLength:testNumber withString:@"1" startingAtIndex:0];
+        NSString *primeTestRegex = @"^1?$|^(11+?)\\1+$";
+        return ![candidate isMatchedByRegex:primeTestRegex];
+    };
+
+    XCTAssertTrue(isPrime(2));
+    XCTAssertTrue(isPrime(7));
+    XCTAssertFalse(isPrime(8));
+    XCTAssertFalse(isPrime(10));
+    XCTAssertFalse(isPrime(12));
+    XCTAssertTrue(isPrime(13));
+    XCTAssertFalse(isPrime(99));
+    XCTAssertFalse(isPrime(100));
+    XCTAssertTrue(isPrime(101));
+    XCTAssertFalse(isPrime(111));
+}
+
 @end
