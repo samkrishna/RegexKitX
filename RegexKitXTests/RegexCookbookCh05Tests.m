eda9dc5d19360c256207cdc35c82e6b74399786b
Sam Krishna <sam.krishna@gmail.com>
2019-10-31 09:39:33 -0700

    [Objective-C] Add RXCB Exercise 5.10

--- RegexKitXTests/RegexCookbookCh05Tests.m
+++ RegexKitXTests/RegexCookbookCh05Tests.m
@@ -129,7 +129,14 @@
 
 - (void)testRegexFromSection510
 {
-    XCTFail(@"Not filled out yet");
+    NSString *regex = @"^.*\\bverbose\\b.*$";
+    NSString *logLines = @""
+    "2019-10-23 23:10:27.252 TickStreamer INFO SFXStreamer.m:107 Hello, World!\n"
+    "2019-10-23 23:10:28.032 TickStreamer VERBOSE SFXStreamer.m:210 -[SFXStreamer registerForDistributedNotifications]\n"
+    "2019-10-23 23:10:28.032 TickStreamer VERBOSE SFXStreamer.m:210 -[SFXStreamer registerForDistributedNotifications]\n"
+    "2019-10-23 23:10:30.209 TickStreamer VERBOSE SFXStreamer.m:1793 -[SFXStreamer getDayOf15MinBarsWithEndDate:forCurrencyPair:]_block_invoke Get 15m AUDJPY bars for day ending 2019-10-23 14:00:01 -0700";
+    NSArray *matches = [logLines rangesOfRegex:regex options:(RKXCaseless | RKXMultiline)];
+    XCTAssertTrue(matches.count == 3);
 }
 
 - (void)testRegexFromSection511
